07430e12b9c985ae16b6f29745ef032498820324
Cz9911 <>
2021-04-26 23:36:35 +0000

    Added a bunch of supercars, sports cars and hyper cars.

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 class Main{
   public static void main(String[] args){
-    Car car1 = new Car("Red", "218 Spyder", 2021);
+    Car car1 = new Car("Red", "Ferrari 218 Spyder", 2021);
     System.out.println(car1);
+    Car car2 = new Car("Black", "RWB", 2021);
+    Car car3 = new Car("Red", "Ferrari 250 GTO", 1964);
+    Car car4 = new Car("Red", "Bugatti Divo", 2019);
+    Car car5 = new Car("Black/Blue", "Bugatti Chiron Supersport", 2019);
+    
   }
   
 }
