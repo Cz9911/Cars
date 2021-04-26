62b7b248ac87dca5abc73492e848c8c676ca2454
Cz9911 <>
2021-04-26 23:28:11 +0000

    Created car class.

---
+++ Car.java
@@ -0,0 +1,39 @@
+class Car {
+  //Fields
+  private String color;
+  private String model;
+  private int year;
+  //Constructor
+  public Car(String color, String model, int year){
+    this.color = color;
+    this.model = model;
+    this.year = year;
+  }
+  //object methods.
+
+  public void brake(){
+   System.out.println("Brake!");
+  }
+  public void accelerate(){
+    System.out.println("Accelerate");
+  }
+  //Getter method
+  public String getColor(){
+    return color;
+  }
+  //setter method
+  public void newColor(String newColor){
+    color = newColor;
+  }
+  //To string!
+  public String toString(){
+    return "This car is "+ color + "And is a " + model;
+
+  }
+
+
+
+
+
+
+  }
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+class Main{
+  public static void main(String[] args){
+    Car car1 = new Car("Red", "218 Spyder", 2021);
+    System.out.println(car1);
   }
+  
 }
