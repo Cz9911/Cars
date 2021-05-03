1bd2329137e9fd0b29b88996714cd5ef178ed2fd
Cz9911 <>
2021-05-03 23:25:55 +0000

    Fixed car toString.

--- Main.java
+++ Main.java
@@ -3,8 +3,11 @@ class Main{
     Car car1 = new Car("Red", "Ferrari 218 Spyder", 2021);
     System.out.println(car1);
     Car car2 = new Car("Black", "RWB", 2021);
+    System.out.println(car2);
     Car car3 = new Car("Red", "Ferrari 250 GTO", 1964);
+    System.out.println(car3);
     Car car4 = new Car("Red", "Bugatti Divo", 2019);
+    System.out.println(car4);
     Car car5 = new Car("Black/Blue", "Bugatti Chiron Supersport", 2019);
     
   }
