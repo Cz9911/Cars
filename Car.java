03ccd497dc260d807e3ef53074e53908b9693dd7
Cz9911 <>
2021-05-10 22:31:29 +0000

    Added horse power and MPH.

--- Car.java
+++ Car.java
@@ -3,23 +3,36 @@ class Car {
   private String color;
   private String model;
   private int year;
+  private double speed;
+  private double hrsPrs;
   //Constructor
-  public Car(String color, String model, int year){
+  public Car(String color, String model, int year, double hrsPrs){
     this.color = color;
     this.model = model;
     this.year = year;
+    this.speed = 0;
+    this.hrsPrs = hrsPrs;
   }
   //object methods.
 
   public void brake(){
    System.out.println("Brake!");
+   speed -= 1;
+   System.out.println(speed + " MPH"); 
   }
+
   public void accelerate(){
     System.out.println("Accelerate");
+    speed += 1;
+    System.out.println(speed + " MPH");
+      
   }
   //Getter method
   public String getColor(){
     return color;
+  }
+  public double getHrsPrs(){
+    return hrsPrs;
   }
   //setter method
   public void newColor(String newColor){
@@ -27,7 +40,7 @@ class Car {
   }
   //To string!
   public String toString(){
-    return "This car is a "+ color + " " + model;
+    return "This car is a "+ color + " " + model+ " " + year + " " + hrsPrs;
 
   }
 
--- Main.java
+++ Main.java
@@ -1,14 +1,23 @@
 class Main{
   public static void main(String[] args){
-    Car car1 = new Car("Red", "Ferrari 218 Spyder", 2021);
+    Car car1 = new Car("Red", "Ferrari 218 Spyder", 2021, 500);
     System.out.println(car1);
-    Car car2 = new Car("Black", "RWB", 2021);
+    Car car2 = new Car("Black", "Custom Porsche RWB", 2021, 397);
     System.out.println(car2);
-    Car car3 = new Car("Red", "Ferrari 250 GTO", 1964);
+    Car car3 = new Car("Red", "Ferrari 250 GTO", 1964, 450);
     System.out.println(car3);
-    Car car4 = new Car("Red", "Bugatti Divo", 2019);
+    Car car4 = new Car("Red", "Bugatti Divo", 2019, 755);
     System.out.println(car4);
-    Car car5 = new Car("Black/Blue", "Bugatti Chiron Supersport", 2019);
+    Car car5 = new Car("Black/Blue", "Bugatti Chiron Supersport", 2019, 748);
+    System.out.println(car5);
+    car1.accelerate();
+    car1.brake();
+    car1.newColor("Yellow");
+    System.out.println(car1);
+    car3.accelerate();
+    car3.accelerate();
+    car3.brake();
+    car3.brake();
     
   }
   
